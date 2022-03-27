8130bcaf5730df16b54b022f2d81e9044fa7352a
mariojuniordev <mario_10gt@hotmail.com>
2022-03-27 01:58:49 -0300

    feat: heading/description of coin page component

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ export const TrendingCoins = (currency: string) =>
 export const CoinList = (currency: string) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
-export const SingleCoin = (id: any) =>
+export const SingleCoin = (id: string | undefined) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const HistoricalChart = (id: string, days = 365, currency: string) =>
