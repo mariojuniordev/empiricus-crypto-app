da576e62cf38a6b34650ef4db40c4c80ee73aba1
mariojuniordev <mario_10gt@hotmail.com>
2022-03-28 01:14:57 -0300

    feat: footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,42 @@
+import { Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 const Footer = () => {
+
+  const useStyles = makeStyles(() => ({
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 15,
+      paddingTop: 10,
+    },
+    link: {
+      margin: 10,
+      "&:hover": {
+        color: "var(--hover-black)",
+        transition: "0.5s",
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <h1>Footer Works!</h1>
+    <footer className={classes.container}>
+      <h3>Developed by Mário Júnior. Reach me on my social Media!</h3>
+      <div>
+        <Link href="https://www.linkedin.com/in/mariojuniordev/">
+          <GitHubIcon fontSize="large" className={classes.link}/>
+        </Link>
+        <Link href="https://github.com/mariojuniordev">
+          <LinkedInIcon fontSize="large" className={classes.link}/>
+        </Link>
+      </div>
+    </footer>
   );
 }
 
